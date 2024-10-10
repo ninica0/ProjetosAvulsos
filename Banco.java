1a9e95e99d3aee8656cacb162a7dddf0c1fc20a6
ninica0 <santosnico048@gmail.com>
2024-10-10 14:55:43 -0300

    feat: Sistema teste de cadastro de usuário

---
+++ Banco.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+public class Banco {
+
+	public static void main(String[] args) {
+	String nome, user;
+	int senha, senha2, loop = 1;
+	
+	nome = JOptionPane.showInputDialog(null, "Digite seu nome completo: ");
+	senha =Integer.parseInt(JOptionPane.showInputDialog(null, "Digite sua senha de 6 números (apenas números): "));
+	
+	JOptionPane.showMessageDialog(null, "Cadastro feito com sucesso! ");
+	
+	do {
+		
+		user = JOptionPane.showInputDialog(null, "Digite seu nome de usuário: ");
+		senha2 =Integer.parseInt(JOptionPane.showInputDialog(null, "Digite sua senha: "));
+		
+		if (user.equals(nome) && (senha2==senha)){
+			JOptionPane.showMessageDialog(null, "Login concluído com sucesso!");
+			loop = 2;
+		}else {
+			JOptionPane.showMessageDialog(null, "Nome ou senha inválido. Tente novamente.");
+		}
+		
+	}while (loop == 1);
+	
+	
+	
+
+	}
+
+}
